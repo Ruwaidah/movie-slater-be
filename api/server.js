9cb4b6304670054b32091286055bf88264d0acaf
ruwaidah <Ruwidah.riyadh@gmail.com>
2020-01-28 16:37:31 -0600

    API for movies with date and zipcode check

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const consumerRouter = require("../auth/consumer/consumer-router");
 const validateUser = require("../auth/auth-helper");
+const movies = require("../movies/movies-router.js");
 
 const server = express();
 
@@ -10,6 +11,7 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 server.use("/api/auth", validateUser, consumerRouter);
+server.use("/api/movies", movies);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "We live" });
