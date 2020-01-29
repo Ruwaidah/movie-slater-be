7618c4f8a1f3448a05d1eb9e7401c41fe080ed95
ruwaidah <Ruwidah.riyadh@gmail.com>
2020-01-29 12:41:06 -0600

    Update the readme File

--- api/server.js
+++ api/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const consumerRouter = require("../auth/consumer/consumer-router");
-const validateUser = require("../auth/auth-helper");
 const movies = require("../movies/movies-router.js");
 
 const server = express();
@@ -10,7 +9,7 @@ const server = express();
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
-server.use("/api/auth", validateUser, consumerRouter);
+server.use("/api/auth", consumerRouter);
 server.use("/api/movies", movies);
 
 server.get("/", (req, res) => {
