f0ec136b58d663b06dcd98e4132f4a4c55ce0750
ruwaidah <Ruwidah.riyadh@gmail.com>
2020-02-06 06:42:45 -0600

    trying to fix CORS issue

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const bodyParser = require("body-parser");
 const consumerRouter = require("../auth/consumer/consumer-router");
 const ownerRouter = require("../auth/owner/owner-router");
 const movies = require("../movies/movies-router.js");
@@ -9,7 +10,6 @@ const seatRouter = require("../seats/seats-router.js");
 const oauth = require("../auth/oauth_consumer/oauth-consumer-router.js");
 
 const server = express();
-server.use(cors());
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"),
     res.header(
@@ -17,19 +17,24 @@ server.use((req, res, next) => {
       "Origin, X-Requested-With, Content-Type, Accept, Authorization"
     );
 
-  req.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+  if (req.method === "OPTIONS") {
+    req.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
 
   next();
 });
 
 server.use(helmet());
 server.use(morgan("dev"));
+server.use(bodyParser.json()).use(bodyParser.urlencoded({ extended: true }));
 server.use(express.json());
+server.use(cors());
 server.use("/api/auth", consumerRouter);
 server.use("/api/auth/owner", ownerRouter);
 server.use("/api/movies", movies);
 server.use("/api/seats", seatRouter);
-server.use("/api/oauth/login", oauth);
+server.use("/api/login", oauth);
 
 server.get("/", (req, res) => {
   res.status(200).json({ message: "We live" });
