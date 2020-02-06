4e55408e4eb8f00a669d49bd1e6fe1208cd93e3c
ruwaidah <Ruwidah.riyadh@gmail.com>
2020-02-05 20:31:44 -0600

    adding header Access-Control-Allow-Origin to server.js

--- api/server.js
+++ api/server.js
@@ -9,6 +9,19 @@ const seatRouter = require("../seats/seats-router.js");
 const oauth = require("../auth/oauth_consumer/oauth-consumer-router.js");
 
 const server = express();
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"),
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+
+  if (req.method === "OPTIONS") {
+    req.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 server.use(helmet());
 server.use(morgan("dev"));
