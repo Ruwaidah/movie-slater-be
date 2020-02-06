5e9396c466104153ac59499580388b772067c2cd
ruwaidah <Ruwidah.riyadh@gmail.com>
2020-02-06 08:27:24 -0600

    trying to fix CORS issue

--- auth/oauth_consumer/restricted-middleware.js
+++ auth/oauth_consumer/restricted-middleware.js
@@ -1,18 +1,42 @@
-const { OAuth2Client } = require("google-auth-library");
-const client = new OAuth2Client(process.env.OAUTH_CLIENT);
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-
-  async function verify() {
-    const ticket = await client.verifyIdToken({
-      idToken: authorization,
-      audience: process.env.OAUTH_CLIENT
+  if (authorization) {
+    const secret = process.env.OAUTH_CLIENT;
+    jwt.verify(authorization, secret, function(err, decodedToken) {
+      if (err) {
+        res.status(401).json({
+          message: "invalid Token"
+        });
+      } else {
+        console.log(decodedToken);
+        req.token = decodedToken;
+        next();
+      }
     });
-    const payload = ticket.getPayload();
-    const userid = payload["sub"];
-    req.user = payload;
-    next();
+  } else {
+    res.status(400).json({
+      message: "Please Login and try again"
+    });
   }
-  verify().catch(console.error);
 };
+
+// const { OAuth2Client } = require("google-auth-library");
+// const client = new OAuth2Client(process.env.OAUTH_CLIENT);
+
+// module.exports = (req, res, next) => {
+//   const { authorization } = req.headers;
+
+//   async function verify() {
+//     const ticket = await client.verifyIdToken({
+//       idToken: authorization,
+//       audience: process.env.OAUTH_CLIENT
+//     });
+//     const payload = ticket.getPayload();
+//     const userid = payload["sub"];
+//     req.user = payload;
+//     next();
+//   }
+//   verify().catch(console.error);
+// };
