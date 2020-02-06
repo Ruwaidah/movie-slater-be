4a073c1ae6fb022431d847800a789dddf4ee943c
ruwaidah <Ruwidah.riyadh@gmail.com>
2020-02-06 08:33:39 -0600

    trying to fix CORS issue

--- auth/oauth_consumer/oauth-consumer-router.js
+++ auth/oauth_consumer/oauth-consumer-router.js
@@ -1,12 +1,9 @@
 const router = require("express").Router();
-const { OAuth2Client } = require("google-auth-library");
 const Consumer = require("./oauth-consumer-model.js");
-const restricted = require("./restricted-middleware.js");
 const axios = require("axios");
 
 // Login With Google Oauth
-router.get("/", (req, res) => {
-  // const client = new OAuth2Client(process.env.OAUTH_CLIENT);
+router.post("/", (req, res) => {
   const { authorization } = req.headers;
   axios
     .get(`https://oauth2.googleapis.com/tokeninfo?id_token=${authorization}`)
@@ -22,22 +19,6 @@ router.get("/", (req, res) => {
           }
         })
         .catch(error => console.log(error));
-      // async function verify() {
-      //   const ticket = await client.verifyIdToken({
-      //     idToken: authorization,
-      //     audience: process.env.OAUTH_CLIENT
-      //   });
-      //   const payload = ticket.getPayload();
-      //   const userid = payload["sub"];
-      //   Consumer.findBy(payload.email).then(consum => {
-      //     if (consum) {
-      //       res.status(200).json(consum);
-      //     } else {
-      //       Consumer.insert(payload).then(resp => res.status(201).json(resp));
-      //     }
-      //   });
-      // }
-      // verify().catch(console.error);
     });
 });
 
