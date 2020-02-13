a18c4ac13f90cdfee80fc073e313e9374eeb1dd6
ruwaidah <Ruwidah.riyadh@gmail.com>
2020-02-13 15:51:30 -0600

    adding page two to upcoming movies

--- movies/comingSoon-router.js
+++ movies/comingSoon-router.js
@@ -2,13 +2,27 @@ const axios = require("axios");
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
+  console.log("page 2");
+
   axios
     .get(
       `
-    https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_APIKEY}&language=en-US&region=us`
+    https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_APIKEY}&language=en-US&page=1&region=us`
     )
     .then(response => {
-      res.status(200).json(response.data.results);
+      console.log(response.data.total_pages);
+      if (response.data.total_pages > 1) {
+        axios
+          .get(
+            `
+        https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_APIKEY}&language=en-US&page=2&region=us`
+          )
+          .then(respon => {
+            res.status(200).json([response.data.results, respon.data.results]);
+          });
+      } else {
+        res.status(200).json([response.data.results]);
+      }
     })
     .catch(error => res.status(500).json({ message: "error geting Data" }));
 });
