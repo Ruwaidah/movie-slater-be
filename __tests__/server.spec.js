3731d845d42b0ebb27d911dd0d42f934709b0b25
ruwaidah <Ruwidah.riyadh@gmail.com>
2020-02-17 22:24:14 -0600

    testing

--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -1,6 +1,5 @@
-const server = require("./server.js");
+const server = require("../api/server.js");
 const request = require("supertest");
-const axios = require("axios");
 
 describe("GET /", () => {
   it("has process .env.DB_ENV as 'development'", () => {
