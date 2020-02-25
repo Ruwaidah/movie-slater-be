7b6ead382c42fd5441c3a284d8cbaa20475db179
ruwaidah <Ruwidah.riyadh@gmail.com>
2020-02-25 12:58:36 -0600

    filter movies for ticket

---
+++ mock-Data/today1917.js
@@ -0,0 +1,279 @@
+module.exports = {
+    data: [{
+        "tmsId": "MV012729530000",
+        "rootId": "17060373",
+        "subType": "Feature Film",
+        "title": "1917",
+        "releaseYear": 2019,
+        "releaseDate": "2019-01-23",
+        "titleLang": "en",
+        "descriptionLang": "en",
+        "entityType": "Movie",
+        "genres": ["War", "Historical drama"],
+        "longDescription": "During World War I, two British soldiers -- Lance Cpl. Schofield and Lance Cpl. Blake -- receive seemingly impossible orders. In a race against time, they must cross over into enemy territory to deliver a message that could potentially save 1,600 of their fellow comrades -- including Blake's own brother.",
+        "shortDescription": "Two British soldiers cross over into enemy territory to save 1,600 comrades during World War I.",
+        "topCast": ["George MacKay", "Dean-Charles Chapman", "Mark Strong"],
+        "directors": ["Sam Mendes"],
+        "officialUrl": "https://www.universalpictures.com/movies/1917",
+        "qualityRating": {
+            "ratingsBody": "TMS",
+            "value": "3.5"
+        },
+        "ratings": [{
+            "body": "Motion Picture Association of America",
+            "code": "R"
+        }],
+        "advisories": ["Adult Language", "Adult Situations", "Violence"],
+        "runTime": "PT01H59M",
+        "preferredImage": {
+            "width": "240",
+            "height": "360",
+            "uri": "assets/p17060373_v_v5_ac.jpg",
+            "category": "VOD Art",
+            "text": "yes",
+            "primary": "true"
+        },
+        "showtimes": [{
+            "theatre": {
+                "id": "5084",
+                "name": "Alamo Drafthouse South Lamar"
+            },
+            "dateTime": "2020-02-25T12:10",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AATHS&m=198853&d=2020-02-25"
+        }, {
+            "theatre": {
+                "id": "5084",
+                "name": "Alamo Drafthouse South Lamar"
+            },
+            "dateTime": "2020-02-25T15:30",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AATHS&m=198853&d=2020-02-25"
+        }, {
+            "theatre": {
+                "id": "5084",
+                "name": "Alamo Drafthouse South Lamar"
+            },
+            "dateTime": "2020-02-25T18:45",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AATHS&m=198853&d=2020-02-25"
+        }, {
+            "theatre": {
+                "id": "5084",
+                "name": "Alamo Drafthouse South Lamar"
+            },
+            "dateTime": "2020-02-25T21:50",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AATHS&m=198853&d=2020-02-25"
+        }, {
+            "theatre": {
+                "id": "5084",
+                "name": "Alamo Drafthouse South Lamar"
+            },
+            "dateTime": "2020-02-26T12:05",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AATHS&m=198853&d=2020-02-26"
+        }, {
+            "theatre": {
+                "id": "5084",
+                "name": "Alamo Drafthouse South Lamar"
+            },
+            "dateTime": "2020-02-26T15:30",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AATHS&m=198853&d=2020-02-26"
+        }, {
+            "theatre": {
+                "id": "5084",
+                "name": "Alamo Drafthouse South Lamar"
+            },
+            "dateTime": "2020-02-26T18:45",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AATHS&m=198853&d=2020-02-26"
+        }, {
+            "theatre": {
+                "id": "5084",
+                "name": "Alamo Drafthouse South Lamar"
+            },
+            "dateTime": "2020-02-26T22:00",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AATHS&m=198853&d=2020-02-26"
+        }, {
+            "theatre": {
+                "id": "5084",
+                "name": "Alamo Drafthouse South Lamar"
+            },
+            "dateTime": "2020-02-27T12:05",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AATHS&m=198853&d=2020-02-27"
+        }, {
+            "theatre": {
+                "id": "5084",
+                "name": "Alamo Drafthouse South Lamar"
+            },
+            "dateTime": "2020-02-27T18:45",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AATHS&m=198853&d=2020-02-27"
+        }, {
+            "theatre": {
+                "id": "5084",
+                "name": "Alamo Drafthouse South Lamar"
+            },
+            "dateTime": "2020-02-27T22:00",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AATHS&m=198853&d=2020-02-27"
+        }, {
+            "theatre": {
+                "id": "5320",
+                "name": "AMC Barton Creek Square 14"
+            },
+            "dateTime": "2020-02-25T10:30",
+            "quals": "Closed Captioned|Descriptive Video Services|Recliners|Reserved Seating",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AANCC&m=198853&d=2020-02-25"
+        }, {
+            "theatre": {
+                "id": "5320",
+                "name": "AMC Barton Creek Square 14"
+            },
+            "dateTime": "2020-02-25T13:15",
+            "quals": "Closed Captioned|Descriptive Video Services|Recliners|Reserved Seating",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AANCC&m=198853&d=2020-02-25"
+        }, {
+            "theatre": {
+                "id": "5320",
+                "name": "AMC Barton Creek Square 14"
+            },
+            "dateTime": "2020-02-25T16:00",
+            "quals": "Closed Captioned|Descriptive Video Services|Recliners|Reserved Seating",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AANCC&m=198853&d=2020-02-25"
+        }, {
+            "theatre": {
+                "id": "5320",
+                "name": "AMC Barton Creek Square 14"
+            },
+            "dateTime": "2020-02-25T18:45",
+            "quals": "Closed Captioned|Descriptive Video Services|Recliners|Reserved Seating",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AANCC&m=198853&d=2020-02-25"
+        }, {
+            "theatre": {
+                "id": "5320",
+                "name": "AMC Barton Creek Square 14"
+            },
+            "dateTime": "2020-02-26T12:10",
+            "quals": "Closed Captioned|Descriptive Video Services|Recliners|Reserved Seating",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AANCC&m=198853&d=2020-02-26"
+        }, {
+            "theatre": {
+                "id": "5320",
+                "name": "AMC Barton Creek Square 14"
+            },
+            "dateTime": "2020-02-26T15:05",
+            "quals": "Closed Captioned|Descriptive Video Services|Recliners|Reserved Seating",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AANCC&m=198853&d=2020-02-26"
+        }, {
+            "theatre": {
+                "id": "5320",
+                "name": "AMC Barton Creek Square 14"
+            },
+            "dateTime": "2020-02-26T18:05",
+            "quals": "Closed Captioned|Descriptive Video Services|Recliners|Reserved Seating",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AANCC&m=198853&d=2020-02-26"
+        }, {
+            "theatre": {
+                "id": "8298",
+                "name": "Regal Westgate Stadium 11"
+            },
+            "dateTime": "2020-02-25T12:30",
+            "quals": "Closed Captioned|Descriptive Video Services",
+            "barg": true,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AANOT&m=198853&d=2020-02-25"
+        }, {
+            "theatre": {
+                "id": "8298",
+                "name": "Regal Westgate Stadium 11"
+            },
+            "dateTime": "2020-02-25T15:30",
+            "quals": "Closed Captioned|Descriptive Video Services",
+            "barg": true,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AANOT&m=198853&d=2020-02-25"
+        }, {
+            "theatre": {
+                "id": "8298",
+                "name": "Regal Westgate Stadium 11"
+            },
+            "dateTime": "2020-02-26T12:30",
+            "quals": "Closed Captioned|Descriptive Video Services",
+            "barg": true,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AANOT&m=198853&d=2020-02-26"
+        }, {
+            "theatre": {
+                "id": "8298",
+                "name": "Regal Westgate Stadium 11"
+            },
+            "dateTime": "2020-02-26T15:30",
+            "quals": "Closed Captioned|Descriptive Video Services",
+            "barg": true,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AANOT&m=198853&d=2020-02-26"
+        }, {
+            "theatre": {
+                "id": "8298",
+                "name": "Regal Westgate Stadium 11"
+            },
+            "dateTime": "2020-02-26T18:30",
+            "quals": "Closed Captioned|Descriptive Video Services",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AANOT&m=198853&d=2020-02-26"
+        }, {
+            "theatre": {
+                "id": "8298",
+                "name": "Regal Westgate Stadium 11"
+            },
+            "dateTime": "2020-02-26T21:30",
+            "quals": "Closed Captioned|Descriptive Video Services",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AANOT&m=198853&d=2020-02-26"
+        }, {
+            "theatre": {
+                "id": "8298",
+                "name": "Regal Westgate Stadium 11"
+            },
+            "dateTime": "2020-02-27T12:30",
+            "quals": "Closed Captioned|Descriptive Video Services",
+            "barg": true,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AANOT&m=198853&d=2020-02-27"
+        }, {
+            "theatre": {
+                "id": "8298",
+                "name": "Regal Westgate Stadium 11"
+            },
+            "dateTime": "2020-02-27T15:30",
+            "quals": "Closed Captioned|Descriptive Video Services",
+            "barg": true,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AANOT&m=198853&d=2020-02-27"
+        }, {
+            "theatre": {
+                "id": "8298",
+                "name": "Regal Westgate Stadium 11"
+            },
+            "dateTime": "2020-02-27T18:30",
+            "quals": "Descriptive Video Services|Closed Captioned",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AANOT&m=198853&d=2020-02-27"
+        }, {
+            "theatre": {
+                "id": "8298",
+                "name": "Regal Westgate Stadium 11"
+            },
+            "dateTime": "2020-02-27T21:30",
+            "quals": "Closed Captioned|Descriptive Video Services",
+            "barg": false,
+            "ticketURI": "http://www.fandango.com/tms.asp?t=AANOT&m=198853&d=2020-02-27"
+        }]
+    }]
+}
